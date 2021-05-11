a5e45a2b74b15abe01d15b6b6d4122f144107b30
Sherry1481 <shareyscorpi1@gmail.com>
2021-05-11 16:50:59 +0500

    Initial Commit

---
+++ Rock Paper Scissor WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Rock Paper Scissor WatchKit Extension
+//
+//  Created by sherry on 11/05/2021.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ Rock Paper Scissor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rock Paper Scissor
+//
+//  Created by sherry on 11/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
